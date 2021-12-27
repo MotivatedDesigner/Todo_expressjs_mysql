39751a8fc5b747400e8cb83f97375d305d4f3d56
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-12-27 11:56:05 +0100

    testing import() to load modules async & dynamically

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
+const port = 7000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  import('./math.js').then(math => {
+    res.send(`hellow ${math.default(3, 2)}`)
+  })
 })
 
 app.listen(port, () => {
